739160d8d87d7bca47541d0f67daef3037bb5b87
mor3245 <inner.super@hotmail.com>
2025-02-11 17:13:55 +0800

    + CatDatabase app + dependency list added requirements.txt - env folder removed

---
+++ CatDatabase/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from CatDatabase.models import Cat
+
+# Register your models here.
+admin.site.register(Cat)
---
+++ CatDatabase/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+djangoUser = get_user_model();  
+
+# Create your models here.
+class Cat(models.Model):
+    CatID = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(djangoUser, on_delete=models.CASCADE)
+    Name = models.CharField(max_length=50)
+    Breed = models.CharField(max_length=50)
+    Age = models.IntegerField()
+    Gender = models.CharField(max_length=1)
+    Description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return str(self.Name)
